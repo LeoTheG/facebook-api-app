1d84acbca1dcc50cedf95ec4462792afdcd62e83
Leonar Gharib <leonargharib@gmail.com>
2018-07-01 12:00:10 -0700

    Working webpack config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import FormContainer from "./components/container/FormContainer";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
